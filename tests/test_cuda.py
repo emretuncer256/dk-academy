c548c216c0f8098d85f926eecd2266f841dc401f
Emre Tuncer <emretuncer.developer@gmail.com>
2024-10-24 21:30:02 +0300

    Add CUDA availability check in test_cuda.py

---
+++ tests/test_cuda.py
@@ -0,0 +1,3 @@
+import torch
+
+assert torch.cuda.is_available(), "CUDA is not available. Please ensure that your system has a CUDA-capable GPU and the correct drivers installed."
--- tests/test_image_generation.py
+++ tests/test_image_generation.py
@@ -5,7 +5,7 @@ from safetensors.torch import load_file
 
 base = "stabilityai/stable-diffusion-xl-base-1.0"
 repo = "ByteDance/SDXL-Lightning"
-ckpt = "sdxl_lightning_4step_unet.safetensors" # Use the correct ckpt for your step setting!
+ckpt = "sdxl_lightning_8step_unet.safetensors" # Use the correct ckpt for your step setting!
 
 # Load model.
 unet = UNet2DConditionModel.from_config(base, subfolder="unet").to("cuda", torch.float16)
@@ -16,4 +16,4 @@ pipe = StableDiffusionXLPipeline.from_pretrained(base, unet=unet, torch_dtype=to
 pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
 
 # Ensure using the same inference steps as the loaded model and CFG set to 0.
-pipe("A girl smiling", num_inference_steps=4, guidance_scale=0).images[0].save("output.png")
+pipe("A girl smiling", num_inference_steps=8, guidance_scale=0).images[0].save("output.png")
