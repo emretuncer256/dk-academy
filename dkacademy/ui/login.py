b8665624e295a733fb4ee169bc68f1449b9adb91
Emirhan Guner <emirhanprogramming@gmail.com>
2024-10-27 13:22:21 +0300

    update login ui about submit

--- dkacademy/ui/login.py
+++ dkacademy/ui/login.py
@@ -5,7 +5,7 @@ def login():
     st.title("DK Akademi ile Maceraya Hazır Mısın?")
     with st.form("login_form"):
         st.header("Kullanıcı Girişi")
-        st.text_input("İsim")
-        st.number_input("Yaş", step=1, min_value=0)
-        st.multiselect("Hangileri ilgini çekiyor?", ["uzay", "doğa", "tatil"])
-        st.form_submit_button("Başla")
+        isim = st.text_input("İsim")
+        yaş = st.number_input("Yaş", step=1, min_value=0)
+        ilgi_alanları = st.multiselect("Hangileri ilgini çekiyor?", ["uzay", "doğa", "tatil"])
+        submit_button = st.form_submit_button("Başla")
