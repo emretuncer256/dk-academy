eb369fa4bbbf78d20d8ca04f253357a2f93a1c11
Emirhan Guner <emirhanprogramming@gmail.com>
2024-10-27 13:45:41 +0300

    update login ui

--- dkacademy/ui/login.py
+++ dkacademy/ui/login.py
@@ -1,11 +1,24 @@
 import streamlit as st
 
+form_values = {
+    "isim" : None,
+    "yaş" : None,
+    "ilgi_alanları" : None, 
+}
 
 def login():
     st.title("DK Akademi ile Maceraya Hazır Mısın?")
-    with st.form("login_form"):
+    with st.form(key="user_info_form"):
         st.header("Kullanıcı Girişi")
-        isim = st.text_input("İsim")
-        yaş = st.number_input("Yaş", step=1, min_value=0)
-        ilgi_alanları = st.multiselect("Hangileri ilgini çekiyor?", ["uzay", "doğa", "tatil"])
-        submit_button = st.form_submit_button("Başla")
+        form_values["isim"] = st.text_input("İsim")
+        form_values["yaş"] = st.number_input("Yaş", step=1, min_value=0)
+        form_values["ilgi_alanları"] = st.multiselect("Hangileri ilgini çekiyor?", ["uzay", "doğa", "tatil"])
+
+        submit_button = st.form_submit_button(label="Başla")
+        if not all(form_values.values()):
+            st.warning("Lütfen tüm bilgileri giriniz!")
+        else:
+            st.balloons()
+            st.write("### Info")
+            for (key, value) in form_values.items():
+                st.write(f"{key}: {value}")
