0f0b8100f070cfa331f436733a1f48b244ba6e39
Emre Tuncer <emretuncer.developer@gmail.com>
2024-10-29 16:22:03 +0300

    Refactor User model to include gender field

--- dkacademy/models/user.py
+++ dkacademy/models/user.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
+from typing import Literal
 
 
 @dataclass
 class User:
     name: str
     age: int
+    gender: Literal["Erkek", "Kız"]
     interests: list[str] | None = None
