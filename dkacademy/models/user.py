edeed0e738893186b47826e118782596a8168474
Emre Tuncer <emretuncer.developer@gmail.com>
2024-10-30 15:25:54 +0300

    Refactor User model to inherit from BaseModel and add JSON serialization method

--- dkacademy/models/user.py
+++ dkacademy/models/user.py
@@ -1,10 +1,19 @@
 from dataclasses import dataclass
+from models.base import BaseModel
 from typing import Literal
 
 
 @dataclass
-class User:
+class User(BaseModel):
     name: str
     age: int
     gender: Literal["Erkek", "Kız"]
     interests: list[str] | None = None
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
+            "interests": self.interests
+        }
