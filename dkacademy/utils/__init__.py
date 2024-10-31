1b90aed80dce25969f1b456e3f49d7b61015fc37
Emre Tuncer <emretuncer.developer@gmail.com>
2024-10-31 19:11:48 +0300

    Add load_lectures function to load lecture data from a JSON file

--- dkacademy/utils/__init__.py
+++ dkacademy/utils/__init__.py
@@ -1,2 +1,2 @@
 from .model import load_model
-from .load_utils import load_interests
+from .load_utils import (load_interests, load_lectures)
--- dkacademy/utils/load_utils.py
+++ dkacademy/utils/load_utils.py
@@ -18,3 +18,20 @@ def load_interests() -> dict:
         st.error(f"An unexpected error occurred: {e}")
         interests = {}
     return interests
+
+
+@st.cache_data
+def load_lectures():
+    try:
+        with open(os.getenv("LECTURES_PATH"), "r") as file:
+            lectures: dict = json.load(file)
+    except FileNotFoundError:
+        st.error("The file interests.json was not found.")
+        lectures = {}
+    except json.JSONDecodeError:
+        st.error("Error decoding the JSON file.")
+        lectures = {}
+    except Exception as e:
+        st.error(f"An unexpected error occurred: {e}")
+        lectures = {}
+    return lectures
