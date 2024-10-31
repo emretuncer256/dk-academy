be71aa1464cab85f7b8d18df73106e58f492be2c
Emre Tuncer <emretuncer.developer@gmail.com>
2024-10-31 12:52:18 +0300

    Initialize prompt in session state if not already set

--- dkacademy/main.py
+++ dkacademy/main.py
@@ -10,6 +10,9 @@ if "page" not in st.session_state:
 if "user" not in st.session_state:
     st.session_state["user"] = None
 
+if "prompt" not in st.session_state:
+    st.session_state["prompt"] = None
+
 pages = {0: login, 1: interests, 2: lessons, 3: topics}
 
 pages[st.session_state["page"]]()
