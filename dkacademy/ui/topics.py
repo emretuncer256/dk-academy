62c2ddaba49c13914a2dbbc50df9fe4a40f8e2eb
Emre Tuncer <emretuncer.developer@gmail.com>
2024-10-31 20:11:43 +0300

    Implement user session check and topic selection in topics.py

--- dkacademy/ui/topics.py
+++ dkacademy/ui/topics.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import utils
 
 
 def topics():
+    if not st.session_state["user"]:
+        st.session_state["page"] = 0
+        st.rerun()
+
+    lesson = st.session_state["prompt"].lesson
+    tps = next(ls["topics"] for ls in utils.load_lessons()["lessons"]
+               if ls["name"] == lesson)
+
     st.title("Topics")
-    st.write(st.session_state["user"])
-    st.write(st.session_state["prompt"])
+
+    for tp in tps:
+        if st.button(tp):
+            st.session_state["prompt"].topic = tp
+            st.session_state["page"] = 4
+            st.rerun()
