b6e63986f07d2540c1ecf25fc80fbf377e3f3e33
Emre Tuncer <emretuncer.developer@gmail.com>
2024-11-01 16:08:10 +0300

    Add generation and content pages to main application flow with session state management

--- dkacademy/main.py
+++ dkacademy/main.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from ui import login_page
-from ui import interests_page
-from ui import lessons_page
-from ui import topics_pages
+from ui import login_page, interests_page, lessons_page, topics_pages, generation_page, content_page
+import utils
+import os
+
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 
 if "page" not in st.session_state:
     st.session_state["page"] = 0
@@ -13,6 +14,22 @@ if "user" not in st.session_state:
 if "prompt" not in st.session_state:
     st.session_state["prompt"] = None
 
-pages = {0: login_page, 1: interests_page, 2: lessons_page, 3: topics_pages}
+if "content" not in st.session_state:
+    st.session_state["content"] = None
+
+if "next_panel" not in st.session_state:
+    st.session_state["next_panel"] = None
+
+if "image_model" not in st.session_state:
+    st.session_state["image_model"] = utils.load_model()
+
+pages = {
+    0: login_page,
+    1: interests_page,
+    2: lessons_page,
+    3: topics_pages,
+    4: generation_page,
+    5: content_page
+}
 
 pages[st.session_state["page"]]()
